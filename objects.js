930b6d03a3f8e55f2843468943e0010d17908bcc
Finn Thye <piqueen@gmail.com>
2019-04-11 14:15:25 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = { artist: 'title' };
 
-function updatePlaylist({ playlist }, artistName, songTitle) {
+function updatePlaylist( playlist, artistName, songTitle) {
   playlist.artistName = songTitle;
   return playlist;
 }
